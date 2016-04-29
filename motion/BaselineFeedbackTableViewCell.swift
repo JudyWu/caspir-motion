72ed537ad19b246bef0a6cf9a6b37ad41315f19b
Zhaoxin Wu <judywu@Zhaoxins-MBP.fios-router.home>
2016-04-29 15:13:25 -0400

    Update

---
+++ motion/BaselineFeedbackTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BaselineFeedbackTableViewCell.swift
+//  motion
+//
+//  Created by Zhaoxin Wu on 4/29/16.
+//  Copyright © 2016 CASPIR. All rights reserved.
+//
+
+import UIKit
+
+class BaselineFeedbackTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
